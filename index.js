17bdae6ec3ebbc5e88154f782c6f53d9099adda4
Collins Kariuki <collinsmunkary@gmail.com>
2023-06-19 10:58:21 +0300

    Setting the views directory

--- index.js
+++ index.js
@@ -4,8 +4,13 @@ const express = require('express');
 //execute express
 const app = express();
 
+//require path
+const path = require('path')
+
 // Tell your app to use EJS as its view engine
 app.set('view engine', 'ejs')
+//this line allows us to run the app from anywhere
+app.set('views', path.join(__dirname, '/views'))
 
 // add home route
 app.get('/', (req,res) => {
